4db60bbb58bb042dfa0b35844c42f55b02504926
Ieber <iebermoura@gmail.com>
2015-09-28 15:05:31 -0300

    tela de setor e funcionario-setor

--- grails-app/domain/br/gov/rn/saogoncalo/protocolo/Protocolo.groovy
+++ grails-app/domain/br/gov/rn/saogoncalo/protocolo/Protocolo.groovy
@@ -13,7 +13,7 @@ class Protocolo {
 	
 	
 	static belongsTo = [tipoDocumento:TipoDocumento , situacao:Situacao]
-	static hasMany = [anexo:Anexo, observacao:Observacao , tramite:Tramite]
+	static hasMany = [anexo:Anexo, tramite:Tramite]
 	
 
 	static constraints = {
